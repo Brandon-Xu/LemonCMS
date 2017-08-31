e1c8423f15784eaa5dc570e3d3ba696d8c04a8b8
BrandonXu <lemonxl1994@gmail.com>
2017-08-31 17:59:42 +0800

    update

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -36,7 +36,59 @@ class SiteController extends FrontController
     }
 
     public function actionIndex() {
-        return $this->render('index');
+        return $this->render('index', []);
+    }
+
+    public function actionLogin() {
+        if (!\app()->user->isGuest) {
+            return $this->goHome();
+        }
+        $model = new User();
+        $model->scenario = 'login';
+        if ($model->load(app()->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+
+        return $this->render('login', [
+            'model' => $model
+        ]);
+    }
+
+    public function actionLogout() {
+        app()->user->logout();
+
+        return $this->goHome();
+    }
+
+    public function actionContact() {
+        //$this->layout=false;
+        $model = new ContactForm();
+        if ($model->load(app()->request->post()) && $model->contact(app()->params[ 'adminEmail' ])) {
+            app()->session->setFlash('contactFormSubmitted');
+
+            return $this->refresh();
+        }
+
+        return $this->render('contact', [
+            'model' => $model
+        ]);
+    }
+
+    public function actionAbout() {
+        return $this->render('about', [
+            'test' => 5,
+            'testData' => $this->testData
+        ]);
+    }
+
+    public function actionClose() {
+        return $this->render('close', [
+            'message' => '站点维护中。。。'
+        ]);
+    }
+
+    public function actionGuestbook() {
+        return $this->render('guestbook', []);
     }
 
 }
