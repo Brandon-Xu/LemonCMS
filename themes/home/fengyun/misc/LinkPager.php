f83104da7cd5384c4570cd85adb38c6e29151899
BrandonXu <lemonxl1994@gmail.com>
2017-09-13 03:18:58 +0800

    balabala

--- themes/home/fengyun/misc/LinkPager.php
+++ themes/home/fengyun/misc/LinkPager.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace themes\home\fengyun\misc;
 
 use yii\helpers\Html;
@@ -6,42 +7,34 @@ use yii\helpers\Html;
 class LinkPager extends \source\core\widgets\LinkPager
 {
 
-    public function init()
-    {
+    public function init() {
         parent::init();
-       
-        $this->options=['id'=>'pagenavi'];
-        $this->linkOptions=['class'=>'page-numbers'];
-        
-        $this->activePageCssClass='current';
+        $this->options = ['id' => 'pagenavi'];
+        $this->linkOptions = ['class' => 'page-numbers'];
+        $this->activePageCssClass = 'current';
     }
 
-    protected function renderPageButtons()
-    {
+    protected function renderPageButtons() {
         $buttons = $this->getButtonItems();
-        if ($buttons === '')
-        {
-            return '';
-        }
-        
+        if ($buttons === '') { return ''; }
+
         return Html::tag('div', implode("\n", $buttons), $this->options);
     }
-    
-    protected function renderPageButton($label, $page, $class, $disabled, $active)
-    {
-        $options = ['class' => $class === '' ? null : $class];
-       
+
+    protected function renderPageButton($label, $page, $class, $disabled, $active) {
+        $options = ['class' => $class === '' ? NULL : $class];
+
         if ($disabled) {
             Html::addCssClass($options, $this->disabledPageCssClass);
             return Html::tag('span', $label);
         }
-        
+
         $linkOptions = $this->linkOptions;
         $linkOptions['data-page'] = $page;
         if ($active) {
             Html::addCssClass($linkOptions, $this->activePageCssClass);
-            //Html::addCssClass($linkOptions, $this->disabledPageCssClass);
         }
+
         return Html::a($label, $this->pagination->createUrl($page), $linkOptions);
     }
 }
